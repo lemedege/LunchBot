03f8bb8d7516a4fa608c04fcc843051a844b82ee
unknown <lassegyrn@gmail.com>
2023-03-06 12:37:40 +0100

    fixed link to get newest week

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ import re
 import datetime
 import os
 
-client = zulip.Client()
+client = zulip.Client(config_file="zuliprc")
  
 BASE_URL =  "https://torvekoekken.dk"
 FAVORIT_URL = "https://torvekoekken.dk/sjaelland/frokostordning/favorit-buffet"
@@ -24,11 +24,11 @@ webpage = requests.get(PORTIONS_URL, headers=HEADERS)
 soup = BeautifulSoup(webpage.content, "html.parser")
 dom = etree.HTML(str(soup))
 urldict = {} 
-urldict["favorit"] = dom.xpath('/html/body/main/div/div/div[3]/div/div[2]/div/div/div/div[2]/button[1]/@onclick')
-urldict["vegetar"] = dom.xpath('/html/body/main/div/div/div[4]/div/div[1]/div/div/div/div[2]/button[1]/@onclick')
-urldict["gluten"] = dom.xpath('/html/body/main/div/div/div[5]/div/div[2]/div/div/div/div[2]/button[1]/@onclick')
-urldict["vegansk"] = dom.xpath('/html/body/main/div/div/div[6]/div/div[1]/div/div/div/div[2]/button[1]/@onclick')
-urldict["halal"] = dom.xpath('/html/body/main/div/div/div[7]/div/div[2]/div/div/div/div[2]/button[1]/@onclick')
+urldict["favorit"] = dom.xpath('/html/body/main/div/div/div[3]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
+urldict["vegetar"] = dom.xpath('/html/body/main/div/div/div[4]/div/div[1]/div/div/div/div[2]/button[2]/@onclick')
+urldict["gluten"] = dom.xpath('/html/body/main/div/div/div[5]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
+urldict["vegansk"] = dom.xpath('/html/body/main/div/div/div[6]/div/div[1]/div/div/div/div[2]/button[2]/@onclick')
+urldict["halal"] = dom.xpath('/html/body/main/div/div/div[7]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
 
 
 # ["location.href='/Files/Files/Branding RAPIDO/Favorit buffet menuer/Favorit buffet uge 9 2023.pdf'"]
@@ -79,5 +79,5 @@ request = {
     "content": todaystext,
 }
 
-result = client.send_message(request)
+#result = client.send_message(request)
 
