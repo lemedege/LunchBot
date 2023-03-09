c58be99fe10cab8db963a7da12bd5ca612ea0dad
unknown <lassegyrn@gmail.com>
2023-03-09 13:07:49 +0100

    moved menus to spoiler tags for each day. fixed error in regex

--- run.py
+++ run.py
@@ -7,7 +7,36 @@ import zulip
 import re
 import datetime
 import os
+import chevron
 
+weekdays = {1: "Mandag",
+            2: "Tirsdag",
+            3: "Onsdag",
+            4: "Torsdag",
+            5: "Fredag",
+            6: "Lørdag",
+            7: "Søndag"}
+            
+emoji_dict = {"favorit": " :piglet: :calf: ", 
+              "vegetar": " :eggplant: :cheese: ", 
+              "vegansk":  " :herb: :apple: ",
+              "glutenfri": " :prohibited: :bread: ",
+              "halal": " :moon: :calf: "}
+
+menudict = {} 
+            
+def render_message(menudict):
+    day_template = """```spoiler {{ day }}\n\n{{content}}```\n"""
+    render = ""
+    for day in range(1,6):
+        content_str = ""
+        for key in menudict:
+            content_str += "##" + emoji_dict[key] + str(key).upper() + emoji_dict[key]
+            content_str += str(menudict[key][day-1])
+        render += chevron.render(day_template, {'day': weekdays[day], 'content': content_str})
+    return(render)
+
+   
 if os.name == 'nt':
     client = zulip.Client(config_file="zuliprc")
 else:
@@ -29,23 +58,13 @@ dom = etree.HTML(str(soup))
 urldict = {} 
 urldict["favorit"] = dom.xpath('/html/body/main/div/div/div[3]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
 urldict["vegetar"] = dom.xpath('/html/body/main/div/div/div[4]/div/div[1]/div/div/div/div[2]/button[2]/@onclick')
-urldict["gluten"] = dom.xpath('/html/body/main/div/div/div[5]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
 urldict["vegansk"] = dom.xpath('/html/body/main/div/div/div[6]/div/div[1]/div/div/div/div[2]/button[2]/@onclick')
+urldict["glutenfri"] = dom.xpath('/html/body/main/div/div/div[5]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
 urldict["halal"] = dom.xpath('/html/body/main/div/div/div[7]/div/div[2]/div/div/div/div[2]/button[2]/@onclick')
-
 
 # ["location.href='/Files/Files/Branding RAPIDO/Favorit buffet menuer/Favorit buffet uge 9 2023.pdf'"]
 # split by qoutation-mark
-
-emoji_dict = {"favorit": " :piglet: :calf: ", 
-              "vegetar": " :eggplant: :cheese: ", 
-              "vegansk":  " :herb: :apple: ",
-              "gluten": " :prohibited: :bread: ",
-              "halal": " :moon: :calf: "}
-    
-menudict = {} 
-   
-   
+ 
 for category,url in urldict.items():
     url = BASE_URL + str(url).split('\'')[1]
     #print(url)
@@ -56,27 +75,12 @@ for category,url in urldict.items():
     for count, page in enumerate(reader.pages):
         text = reader.pages[count].extract_text()
         text = re.sub(r'\(.*\)', '', text) # remove allergene markings
-        text = re.sub(r'.*([a-åA-å]{3,4}[DAG])\w+', '', text) # remove allergene markings
+        text = re.sub(r'.*[a-åA-å]{3,4}\w*DAG\w*', '', text) # remove days
         text = text.split('Tallene')[0] #remove allergene text at the end of string
         menudict[category].append(text)
-    
-  
-
-today = datetime.datetime.now()
-todaystext = "# " + today.strftime("%A").upper() + " WEEK " + today.strftime("%W")
-todaystext += ("\n---\n")
-
-
-for key in menudict:
-    text = "##" + emoji_dict[key] + str(key).upper() + emoji_dict[key]
-    # text += '\n'
-    text += str(menudict[key][today.weekday()])
-    text += '\n'
-    todaystext += (text)
-
+todaystext = render_message(menudict)    
 
 stream_name = os.environ.get("STREAM")
-
 print("sending menu of the day to:")
 print(stream_name)
 
@@ -89,16 +93,13 @@ if os.name == 'nt':
     "content": todaystext,
     }
     
-
 else:
     request = {
         "type": "stream",
         "to": stream_name,
-        "topic": "Week "+ today.strftime("%W"),
+        "topic": "Uge "+ (today + datetime.timedelta(days=7)).strftime("%W"),
         "content": todaystext,
     }
-
-
+    
 result = client.send_message(request)
 print(result)
-
